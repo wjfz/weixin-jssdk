d1d5aaa8bea427535af8af4e7c221b74447108a6
wjfz <wjfz0328@qq.com>
2015-01-10 04:21:19 +0800

    彻底调通可用

--- index.php
+++ index.php
@@ -1,24 +1,15 @@
 <?php
 
 $appId = 'YOUR_APPID';
-$url = 'http://slgxzs.sinaapp.com/jssdk/index.php';		// url记得要在微信后台配置
-$jsapi_ticket = 'bxLdikRXVbTPdHSM05e5uyVZ3ffhe_UwghujcQF_hnYBceUbudPhL1WsmC5AG3hPBkb5LwlVv3c4LjiitKFm8w';
-// jsapi_ticket，生成详见文档。由于是测试就直接写死了。
+$appsecret = 'YOUR_APPSECRET';
 
-$nonceStr = make_nonceStr();
 $timestamp = time();
+$jsapi_ticket = make_ticket($appId,$appsecret);
+$nonceStr = make_nonceStr();
+$url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+$signature = make_signature($nonceStr,$timestamp,$jsapi_ticket,$url);
 
-$tmpArr = array(
-	'nonceStr' => $nonceStr,
-	'timestamp' => $timestamp,
-	'jsapi_ticket' => $jsapi_ticket,
-	'url' => $url
-);
-ksort($tmpArr, SORT_STRING);
-$tmpStr = http_build_query( $tmpArr );
-$tmpStr = sha1( $tmpStr );
-
-public function make_nonceStr()
+function make_nonceStr()
 {
 	$codeSet = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 	for ($i = 0; $i<16; $i++) {
@@ -28,172 +19,204 @@ public function make_nonceStr()
 	return $nonceStr;
 }
 
+function make_signature($nonceStr,$timestamp,$jsapi_ticket,$url)
+{
+	$tmpArr = array(
+	'noncestr' => $nonceStr,
+	'timestamp' => $timestamp,
+	'jsapi_ticket' => $jsapi_ticket,
+	'url' => $url
+	);
+	ksort($tmpArr, SORT_STRING);
+	$string1 = http_build_query( $tmpArr );
+	$string1 = urldecode( $string1 );
+	$signature = sha1( $string1 );
+	return $signature;
+}
+
+function make_ticket($appId,$appsecret)
+{
+	// 这里要对tiket进行检测，避免api请求超额
+	$TOKEN_URL="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appId."&secret=".$appsecret;
+	$json = file_get_contents($TOKEN_URL);
+	$result = json_decode($json,true);
+	$access_token = $result['access_token'];
+
+	$ticket_URL="https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=".$access_token."&type=jsapi";
+	$json = file_get_contents($ticket_URL);
+	$result = json_decode($json,true);
+	$ticket = $result['ticket'];
+	// 这里要对tiket进行缓存操作
+
+	return $ticket;
+}
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <title>微信JS-SDK Demo</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
-  <link rel="stylesheet" href="style.css">
+	<meta charset="utf-8">
+	<title>微信JS-SDK Demo</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body ontouchstart="">
 <div class="wxapi_container">
 	<div class="wxapi_index_container">
-	  <ul class="label_box lbox_close wxapi_index_list">
-		<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-basic">基础接口</a></li>
-		<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-share">分享接口</a></li>
-		<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-image">图像接口</a></li>
-		<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-voice">音频接口</a></li>
-		<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-smart">智能接口</a></li>
-		<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-device">设备信息接口</a></li>
-		<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-location">地理位置接口</a></li>
-		<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-webview">界面操作接口</a></li>
-		<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-scan">微信扫一扫接口</a></li>
-		<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-shopping">微信小店接口</a></li>
-		<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-card">微信卡券接口</a></li>
-		<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-pay">微信支付接口</a></li>
-	  </ul>
+	<ul class="label_box lbox_close wxapi_index_list">
+	<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-basic">基础接口</a></li>
+	<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-share">分享接口</a></li>
+	<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-image">图像接口</a></li>
+	<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-voice">音频接口</a></li>
+	<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-smart">智能接口</a></li>
+	<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-device">设备信息接口</a></li>
+	<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-location">地理位置接口</a></li>
+	<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-webview">界面操作接口</a></li>
+	<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-scan">微信扫一扫接口</a></li>
+	<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-shopping">微信小店接口</a></li>
+	<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-card">微信卡券接口</a></li>
+	<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-pay">微信支付接口</a></li>
+	</ul>
 	</div>
 	<div class="lbox_close wxapi_form">
-	  <h3 id="menu-basic">基础接口</h3>
-	  <span class="desc">判断当前客户端是否支持指定JS接口</span>
-	  <button class="btn btn_primary" id="checkJsApi">checkJsApi</button>
+	<h3 id="menu-basic">基础接口</h3>
+	<span class="desc">判断当前客户端是否支持指定JS接口</span>
+	<button class="btn btn_primary" id="checkJsApi">checkJsApi</button>
 
-	  <h3 id="menu-share">分享接口</h3>
-	  <span class="desc">获取“分享到朋友圈”按钮点击状态及自定义分享内容接口</span>
-	  <button class="btn btn_primary" id="onMenuShareTimeline">onMenuShareTimeline</button>
-	  <span class="desc">获取“分享给朋友”按钮点击状态及自定义分享内容接口</span>
-	  <button class="btn btn_primary" id="onMenuShareAppMessage">onMenuShareAppMessage</button>
-	  <span class="desc">获取“分享到QQ”按钮点击状态及自定义分享内容接口</span>
-	  <button class="btn btn_primary" id="onMenuShareQQ">onMenuShareQQ</button>
-	  <span class="desc">获取“分享到腾讯微博”按钮点击状态及自定义分享内容接口</span>
-	  <button class="btn btn_primary" id="onMenuShareWeibo">onMenuShareWeibo</button>
+	<h3 id="menu-share">分享接口</h3>
+	<span class="desc">获取“分享到朋友圈”按钮点击状态及自定义分享内容接口</span>
+	<button class="btn btn_primary" id="onMenuShareTimeline">onMenuShareTimeline</button>
+	<span class="desc">获取“分享给朋友”按钮点击状态及自定义分享内容接口</span>
+	<button class="btn btn_primary" id="onMenuShareAppMessage">onMenuShareAppMessage</button>
+	<span class="desc">获取“分享到QQ”按钮点击状态及自定义分享内容接口</span>
+	<button class="btn btn_primary" id="onMenuShareQQ">onMenuShareQQ</button>
+	<span class="desc">获取“分享到腾讯微博”按钮点击状态及自定义分享内容接口</span>
+	<button class="btn btn_primary" id="onMenuShareWeibo">onMenuShareWeibo</button>
 
-	  <h3 id="menu-image">图像接口</h3>
-	  <span class="desc">拍照或从手机相册中选图接口</span>
-	  <button class="btn btn_primary" id="chooseImage">chooseImage</button>
-	  <span class="desc">预览图片接口</span>
-	  <button class="btn btn_primary" id="previewImage">previewImage</button>
-	  <span class="desc">上传图片接口</span>
-	  <button class="btn btn_primary" id="uploadImage">uploadImage</button>
-	  <span class="desc">下载图片接口</span>
-	  <button class="btn btn_primary" id="downloadImage">downloadImage</button>
+	<h3 id="menu-image">图像接口</h3>
+	<span class="desc">拍照或从手机相册中选图接口</span>
+	<button class="btn btn_primary" id="chooseImage">chooseImage</button>
+	<span class="desc">预览图片接口</span>
+	<button class="btn btn_primary" id="previewImage">previewImage</button>
+	<span class="desc">上传图片接口</span>
+	<button class="btn btn_primary" id="uploadImage">uploadImage</button>
+	<span class="desc">下载图片接口</span>
+	<button class="btn btn_primary" id="downloadImage">downloadImage</button>
 
-	  <h3 id="menu-voice">音频接口</h3>
-	  <span class="desc">开始录音接口</span>
-	  <button class="btn btn_primary" id="startRecord">startRecord</button>
-	  <span class="desc">停止录音接口</span>
-	  <button class="btn btn_primary" id="stopRecord">stopRecord</button>
-	  <span class="desc">播放语音接口</span>
-	  <button class="btn btn_primary" id="playVoice">playVoice</button>
-	  <span class="desc">暂停播放接口</span>
-	  <button class="btn btn_primary" id="pauseVoice">pauseVoice</button>
-	  <span class="desc">停止播放接口</span>
-	  <button class="btn btn_primary" id="stopVoice">stopVoice</button>
-	  <span class="desc">上传语音接口</span>
-	  <button class="btn btn_primary" id="uploadVoice">uploadVoice</button>
-	  <span class="desc">下载语音接口</span>
-	  <button class="btn btn_primary" id="downloadVoice">downloadVoice</button>
+	<h3 id="menu-voice">音频接口</h3>
+	<span class="desc">开始录音接口</span>
+	<button class="btn btn_primary" id="startRecord">startRecord</button>
+	<span class="desc">停止录音接口</span>
+	<button class="btn btn_primary" id="stopRecord">stopRecord</button>
+	<span class="desc">播放语音接口</span>
+	<button class="btn btn_primary" id="playVoice">playVoice</button>
+	<span class="desc">暂停播放接口</span>
+	<button class="btn btn_primary" id="pauseVoice">pauseVoice</button>
+	<span class="desc">停止播放接口</span>
+	<button class="btn btn_primary" id="stopVoice">stopVoice</button>
+	<span class="desc">上传语音接口</span>
+	<button class="btn btn_primary" id="uploadVoice">uploadVoice</button>
+	<span class="desc">下载语音接口</span>
+	<button class="btn btn_primary" id="downloadVoice">downloadVoice</button>
 
-	  <h3 id="menu-smart">智能接口</h3>
-	  <span class="desc">识别音频并返回识别结果接口</span>
-	  <button class="btn btn_primary" id="translateVoice">translateVoice</button>
+	<h3 id="menu-smart">智能接口</h3>
+	<span class="desc">识别音频并返回识别结果接口</span>
+	<button class="btn btn_primary" id="translateVoice">translateVoice</button>
 
-	  <h3 id="menu-device">设备信息接口</h3>
-	  <span class="desc">获取网络状态接口</span>
-	  <button class="btn btn_primary" id="getNetworkType">getNetworkType</button>
+	<h3 id="menu-device">设备信息接口</h3>
+	<span class="desc">获取网络状态接口</span>
+	<button class="btn btn_primary" id="getNetworkType">getNetworkType</button>
 
-	  <h3 id="menu-location">地理位置接口</h3>
-	  <span class="desc">使用微信内置地图查看位置接口</span>
-	  <button class="btn btn_primary" id="openLocation">openLocation</button>
-	  <span class="desc">获取地理位置接口</span>
-	  <button class="btn btn_primary" id="getLocation">getLocation</button>
+	<h3 id="menu-location">地理位置接口</h3>
+	<span class="desc">使用微信内置地图查看位置接口</span>
+	<button class="btn btn_primary" id="openLocation">openLocation</button>
+	<span class="desc">获取地理位置接口</span>
+	<button class="btn btn_primary" id="getLocation">getLocation</button>
 
-	  <h3 id="menu-webview">界面操作接口</h3>
-	  <span class="desc">隐藏右上角菜单接口</span>
-	  <button class="btn btn_primary" id="hideOptionMenu">hideOptionMenu</button>
-	  <span class="desc">显示右上角菜单接口</span>
-	  <button class="btn btn_primary" id="showOptionMenu">showOptionMenu</button>
-	  <span class="desc">关闭当前网页窗口接口</span>
-	  <button class="btn btn_primary" id="closeWindow">closeWindow</button>
-	  <span class="desc">批量隐藏功能按钮接口</span>
-	  <button class="btn btn_primary" id="hideMenuItems">hideMenuItems</button>
-	  <span class="desc">批量显示功能按钮接口</span>
-	  <button class="btn btn_primary" id="showMenuItems">showMenuItems</button>
-	  <span class="desc">隐藏所有非基础按钮接口</span>
-	  <button class="btn btn_primary" id="hideAllNonBaseMenuItem">hideAllNonBaseMenuItem</button>
-	  <span class="desc">显示所有功能按钮接口</span>
-	  <button class="btn btn_primary" id="showAllNonBaseMenuItem">showAllNonBaseMenuItem</button>
+	<h3 id="menu-webview">界面操作接口</h3>
+	<span class="desc">隐藏右上角菜单接口</span>
+	<button class="btn btn_primary" id="hideOptionMenu">hideOptionMenu</button>
+	<span class="desc">显示右上角菜单接口</span>
+	<button class="btn btn_primary" id="showOptionMenu">showOptionMenu</button>
+	<span class="desc">关闭当前网页窗口接口</span>
+	<button class="btn btn_primary" id="closeWindow">closeWindow</button>
+	<span class="desc">批量隐藏功能按钮接口</span>
+	<button class="btn btn_primary" id="hideMenuItems">hideMenuItems</button>
+	<span class="desc">批量显示功能按钮接口</span>
+	<button class="btn btn_primary" id="showMenuItems">showMenuItems</button>
+	<span class="desc">隐藏所有非基础按钮接口</span>
+	<button class="btn btn_primary" id="hideAllNonBaseMenuItem">hideAllNonBaseMenuItem</button>
+	<span class="desc">显示所有功能按钮接口</span>
+	<button class="btn btn_primary" id="showAllNonBaseMenuItem">showAllNonBaseMenuItem</button>
 
-	  <h3 id="menu-scan">微信扫一扫</h3>
-	  <span class="desc">调起微信扫一扫接口</span>
-	  <button class="btn btn_primary" id="scanQRCode0">scanQRCode(微信处理结果)</button>
-	  <button class="btn btn_primary" id="scanQRCode1">scanQRCode(直接返回结果)</button>
+	<h3 id="menu-scan">微信扫一扫</h3>
+	<span class="desc">调起微信扫一扫接口</span>
+	<button class="btn btn_primary" id="scanQRCode0">scanQRCode(微信处理结果)</button>
+	<button class="btn btn_primary" id="scanQRCode1">scanQRCode(直接返回结果)</button>
 
-	  <h3 id="menu-shopping">微信小店接口</h3>
-	  <span class="desc">跳转微信商品页接口</span>
-	  <button class="btn btn_primary" id="openProductSpecificView">openProductSpecificView</button>
+	<h3 id="menu-shopping">微信小店接口</h3>
+	<span class="desc">跳转微信商品页接口</span>
+	<button class="btn btn_primary" id="openProductSpecificView">openProductSpecificView</button>
 
-	  <h3 id="menu-card">微信卡券接口</h3>
-	  <span class="desc">批量添加卡券接口</span>
-	  <button class="btn btn_primary" id="addCard">addCard</button>
-	  <span class="desc">调起适用于门店的卡券列表并获取用户选择列表</span>
-	  <button class="btn btn_primary" id="chooseCard">chooseCard</button>
-	  <span class="desc">查看微信卡包中的卡券接口</span>
-	  <button class="btn btn_primary" id="openCard">openCard</button>
+	<h3 id="menu-card">微信卡券接口</h3>
+	<span class="desc">批量添加卡券接口</span>
+	<button class="btn btn_primary" id="addCard">addCard</button>
+	<span class="desc">调起适用于门店的卡券列表并获取用户选择列表</span>
+	<button class="btn btn_primary" id="chooseCard">chooseCard</button>
+	<span class="desc">查看微信卡包中的卡券接口</span>
+	<button class="btn btn_primary" id="openCard">openCard</button>
 
-	  <h3 id="menu-pay">微信支付接口</h3>
-	  <span class="desc">发起一个微信支付请求</span>
-	  <button class="btn btn_primary" id="chooseWXPay">chooseWXPay</button>
+	<h3 id="menu-pay">微信支付接口</h3>
+	<span class="desc">发起一个微信支付请求</span>
+	<button class="btn btn_primary" id="chooseWXPay">chooseWXPay</button>
 	</div>
-  </div>
+	</div>
 </body>
 <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 <script>
-  wx.config({
-	  debug: true,
-	  appId: '<?=$appId?>',
-	  timestamp: <?=$timestamp?>,
-	  nonceStr: '<?=$nonceStr?>',
-	  signature: '<?=$tmpStr?>',
-	  jsApiList: [
-		'checkJsApi',
-		'onMenuShareTimeline',
-		'onMenuShareAppMessage',
-		'onMenuShareQQ',
-		'onMenuShareWeibo',
-		'hideMenuItems',
-		'showMenuItems',
-		'hideAllNonBaseMenuItem',
-		'showAllNonBaseMenuItem',
-		'translateVoice',
-		'startRecord',
-		'stopRecord',
-		'onRecordEnd',
-		'playVoice',
-		'pauseVoice',
-		'stopVoice',
-		'uploadVoice',
-		'downloadVoice',
-		'chooseImage',
-		'previewImage',
-		'uploadImage',
-		'downloadImage',
-		'getNetworkType',
-		'openLocation',
-		'getLocation',
-		'hideOptionMenu',
-		'showOptionMenu',
-		'closeWindow',
-		'scanQRCode',
-		'chooseWXPay',
-		'openProductSpecificView',
-		'addCard',
-		'chooseCard',
-		'openCard'
-	  ]
-  });
+	wx.config({
+	debug: true,
+	appId: '<?=$appId?>',
+	timestamp: <?=$timestamp?>,
+	nonceStr: '<?=$nonceStr?>',
+	signature: '<?=$signature?>',
+	jsApiList: [
+	'checkJsApi',
+	'onMenuShareTimeline',
+	'onMenuShareAppMessage',
+	'onMenuShareQQ',
+	'onMenuShareWeibo',
+	'hideMenuItems',
+	'showMenuItems',
+	'hideAllNonBaseMenuItem',
+	'showAllNonBaseMenuItem',
+	'translateVoice',
+	'startRecord',
+	'stopRecord',
+	'onRecordEnd',
+	'playVoice',
+	'pauseVoice',
+	'stopVoice',
+	'uploadVoice',
+	'downloadVoice',
+	'chooseImage',
+	'previewImage',
+	'uploadImage',
+	'downloadImage',
+	'getNetworkType',
+	'openLocation',
+	'getLocation',
+	'hideOptionMenu',
+	'showOptionMenu',
+	'closeWindow',
+	'scanQRCode',
+	'chooseWXPay',
+	'openProductSpecificView',
+	'addCard',
+	'chooseCard',
+	'openCard'
+	]
+	});
 </script>
-<script src="api-6.1.js?v=1"> </script>
+<script src="api-6.1.js"> </script>
 </html>
